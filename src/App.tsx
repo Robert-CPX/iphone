64a24e473d7e2fd11be9b5a3d87be6dd2444185c
Robert-CPX <cpx@robbdev.xyz>
2024-03-18 21:47:52 +0800

    setup project, add tailwindcss, fix npm run dev error

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="text-5xl font-bold">
+        <h1>React TypeScript App</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App;
