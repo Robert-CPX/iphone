a08ad1d5391c7898dfed116dc286ebd8f1255c47
Robert-CPX <cpx@robbdev.xyz>
2024-03-19 14:35:36 +0800

    hero and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Hero, Navbar, Highlights } from "./components";
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="text-5xl font-bold">
-        <h1>React TypeScript App</h1>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
